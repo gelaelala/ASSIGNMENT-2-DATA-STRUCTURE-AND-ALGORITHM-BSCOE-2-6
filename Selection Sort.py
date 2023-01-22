98d88b1e8091b63adacbaea0aa8085d12de63119
unknown <angelaescariocorpuz@gmail.com>
2023-01-22 15:58:56 +0800

    created the first loop needed in the sorting algorithm

--- Selection Sort.py
+++ Selection Sort.py
@@ -1,5 +1,7 @@
 # defined function for the Selection Sort algorithm
 def selection_sort (nums):
-    
+    for i in range (9):
+        min_position = i # this will hold the min position in the array
 
+# assigned array values to be sorted using the algorithm
 nums = [79, 33, 95, 54, 97, 11, 90, 89, 64, 19]
