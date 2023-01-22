f8ccbc73f13be21feba5d9e6909c4c1d2bf909ee
unknown <angelaescariocorpuz@gmail.com>
2023-01-22 16:04:42 +0800

    added second loop in the function

--- Selection Sort.py
+++ Selection Sort.py
@@ -2,6 +2,11 @@
 def selection_sort (nums):
     for i in range (9):
         min_position = i # this will hold the min position in the array
+        for j in range (i, 10):
+            if nums [j] < nums [min_position]:
+                min_position = j # this will change the min position once the loop finds a value lower than the current min position
+
+        # for swapping the values inside the array
 
 # assigned array values to be sorted using the algorithm
 nums = [79, 33, 95, 54, 97, 11, 90, 89, 64, 19]
