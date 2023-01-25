bca68763044945f60de6b70dd8d348da429710a5
unknown <angelaescariocorpuz@gmail.com>
2023-01-26 06:41:12 +0800

    added for loop inside the function for comparing value to the left of it

--- Insertion Sort.py
+++ Insertion Sort.py
@@ -2,6 +2,12 @@
 
 # defined function for the Insertion Sort algorithm
 def insertion_sort (nums):
+    for index in range (1, len(list)):
+        value = list[index]
+        i = index - 1 # in order to compare the value to the left of it
+
+
+
 
 # assigned values to be sorted out using the algorithm
 assigned_array = [79, 33, 95, 54, 97, 11, 90, 89, 64, 19]
