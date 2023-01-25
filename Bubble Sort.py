c0ddf55bf48801591c8cdafcd6b6e475e389703d
unknown <angelaescariocorpuz@gmail.com>
2023-01-26 05:35:24 +0800

    added loop for the outer and inner loop as well as a condition for swapping of values

--- Bubble Sort.py
+++ Bubble Sort.py
@@ -1,4 +1,13 @@
 # --- Bubble Sort Algorithm (Reference: https://youtu.be/Vca808JTbI8) ---
 
+# defined function for the Bubble Sort algorithm
+def bubble_sort (nums):
+    
+    for i in range (len(nums)-1, 0, -1):
+        for j in range (i): 
+            if nums[j] > nums [j+1]:
+                nums[j], nums[j+1] = nums[j+1], nums[j] #another way of swapping the variables without taking a temp variable
+
+# assigned values to be sorted using the algorithm
 assigned_array = [79, 33, 95, 54, 97, 11, 90, 89, 64, 19]
-print ("Unsorted list: ", assigned_array)
+print ("Unsorted list: ", assigned_array) # prints out the unsorted array only 
