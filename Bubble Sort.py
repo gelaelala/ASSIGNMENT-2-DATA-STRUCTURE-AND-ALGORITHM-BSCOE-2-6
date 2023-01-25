2c65f9404b49fe31a301e73dc9677aa9e902ed7c
unknown <angelaescariocorpuz@gmail.com>
2023-01-26 06:10:24 +0800

    added codes to print out the sorted array, array after each iteration, and values to be swapped; also added a code to call the function bubble_sort

--- Bubble Sort.py
+++ Bubble Sort.py
@@ -3,11 +3,20 @@
 # defined function for the Bubble Sort algorithm
 def bubble_sort (nums):
     
-    for i in range (len(nums)-1, 0, -1):
-        for j in range (i): 
-            if nums[j] > nums [j+1]:
+    for i in range (len(nums)-1, 0, -1): # outer loop
+        for j in range (i): # inner loop
+            if nums[j] > nums [j+1]: # to determine whether there's a need to swap the values or not
                 nums[j], nums[j+1] = nums[j+1], nums[j] #another way of swapping the variables without taking a temp variable
+                print (f"Since {nums[j+1]} is greater than {nums[j]}, swap") # to check which values are swapped during each iteration
+
+        print (f"Iteration {j}:", nums) # prints out the entire array after every swapping iteration 
 
 # assigned values to be sorted using the algorithm
 assigned_array = [79, 33, 95, 54, 97, 11, 90, 89, 64, 19]
-print ("Unsorted list: ", assigned_array) # prints out the unsorted array only 
+
+# prints out the unsorted array only 
+print ("Unsorted list:", assigned_array) 
+bubble_sort (assigned_array)
+
+#prints out the final sorted array after going through the swapping iterations
+print ("Sorted list using Bubble Sort:", assigned_array)
