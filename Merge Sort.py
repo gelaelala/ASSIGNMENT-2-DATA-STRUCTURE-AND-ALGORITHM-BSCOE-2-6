e3f6828ad84d04db3ec22d30dbbbc0a3c307bc08
unknown <angelaescariocorpuz@gmail.com>
2023-01-26 11:50:16 +0800

    called merge_sort function for the recursion part for both left and right list

--- Merge Sort.py
+++ Merge Sort.py
@@ -6,6 +6,10 @@ def merge_sort (list):
         left_list = list[:len(list)//2] # sub array that goes from the beginning of the original array to the middle point
         right_list = list[len(list)//2:] # sub array that goes from the middle point to the end of the original array  
 
+        # for the recursion on both left and right list
+        merge_sort (left_list)
+        merge_sort (right_list)
+
 
 # assigned values to be sorted out using the algorithm
 assigned_array = [79, 33, 95, 54, 97, 11, 90, 89, 64, 19]
