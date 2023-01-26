2627a60560faf44d42a9acc0e768fae5c23ecec6
unknown <angelaescariocorpuz@gmail.com>
2023-01-26 11:46:13 +0800

    added defined function for the sorting algorithm and an if statement for splitting the array in half

--- Merge Sort.py
+++ Merge Sort.py
@@ -1,3 +1,11 @@
 # --- Merge Sort Algorithm (Reference: https://youtu.be/cVZMah9kEjI) ---
 
+# defined function for the Merge Sort algorithm
+def merge_sort (list):
+    if len (list) > 1: # in order to split the array in half
+        left_list = list[:len(list)//2] # sub array that goes from the beginning of the original array to the middle point
+        right_list = list[len(list)//2:] # sub array that goes from the middle point to the end of the original array  
+
+
+# assigned values to be sorted out using the algorithm
 assigned_array = [79, 33, 95, 54, 97, 11, 90, 89, 64, 19]
