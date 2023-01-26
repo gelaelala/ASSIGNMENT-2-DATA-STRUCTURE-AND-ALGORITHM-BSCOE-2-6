b9a99fd9785f0bdd236b4b5b30b3e8eae562400e
unknown <angelaescariocorpuz@gmail.com>
2023-01-27 06:35:41 +0800

    added while loop to check if i and j crossed and another while loop for the index i to move to the right and index j to the left

--- Quick Sort.py
+++ Quick Sort.py
@@ -10,7 +10,16 @@ def quick_sort (arr, left, right): # left and right are indexes that determine t
 def partition (arr, left, right): # returns the index of the pivot element 
     i = left # left point of the area to sort
     j = right - 1 # point right of the pivot
-    pivot = arr[right] # pivot itself
+    pivot = arr[right] # pivot element itself
+
+    while i < j: # to check if i and j crossed
+        while i < right and arr[i] < pivot: # while i is not at the end of the array and the element at index i is less than the pivot element
+            i += 1 # will move i to the right 
+
+        while j > left and arr[j] >= pivot:
+            j -= 1 # this will move j to the left
+        
+        if i < j: # to check if i and j have crossed yet
 
 # assigned values to be sorted out using the algorithm
 assigned_array = [79, 33, 95, 54, 97, 11, 90, 89, 64, 19]
