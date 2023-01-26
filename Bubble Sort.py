d6977eb8cb6e3ec6574ccbe7f2f630988616df2c
unknown <angelaescariocorpuz@gmail.com>
2023-01-26 12:39:54 +0800

    fixed the format under print functions

--- Bubble Sort.py
+++ Bubble Sort.py
@@ -16,7 +16,8 @@ assigned_array = [79, 33, 95, 54, 97, 11, 90, 89, 64, 19]
 
 # prints out the unsorted array only 
 print ("Unsorted list:", assigned_array) 
+print ("\n")
 bubble_sort (assigned_array)
 
 #prints out the final sorted array after going through the swapping iterations
-print ("Sorted list using Bubble Sort:", assigned_array)
+print ("\nSorted list using Bubble Sort:", assigned_array)
