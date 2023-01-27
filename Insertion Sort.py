dcbd730a9894e06bf46e17d2342c02e4a4fb5741
unknown <angelaescariocorpuz@gmail.com>
2023-01-27 08:33:47 +0800

    minor revisions only

--- Insertion Sort.py
+++ Insertion Sort.py
@@ -9,6 +9,9 @@ def insertion_sort (list):
             if value < list [i]: # if the statement is true, it will shift list[i] to the right
                 list [i+1] = list[i] # shift the number in slot i right to slot i+1
                 list[i] = value # shift value left into slot i
+
+                print (f"\nSwap {list[i+1]} with {list[i]}")
+
                 i = i - 1 # to decrement i in order to compare value to something lower
             else: 
                 break # if the value is not less than the item being compared to, meaning the value is in the right place already
@@ -20,7 +23,6 @@ assigned_array = [79, 33, 95, 54, 97, 11, 90, 89, 64, 19]
 
 # prints out the unsorted array only
 print ("Unsorted list:", assigned_array)
-print ("\n")
 insertion_sort (assigned_array)
 
 # prints out the sorted array
